d924db5cab324fc18d7480e8a739ffeff09758e4
Zavire Shiran <zavire.shiran@gmail.com>
2017-12-05 20:25:27 -0600

    add loop and break on eof

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,17 @@ use std::io::{Write};
 
 fn main() {
     let mut stdout = io::stdout();
-    stdout.write(b"$ ").expect("");
-    stdout.flush().expect("");
 
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("");
-    stdout.write(input.as_bytes()).expect("");
+    loop {
+        stdout.write(b"$ ").expect("");
+        stdout.flush().expect("");
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("");
+        if input.len() == 0 {
+            stdout.write(b"\n").is_ok();
+            break
+        }
+        stdout.write(input.as_bytes()).expect("");
+    }
 }
