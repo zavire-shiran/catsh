82f9e0ed7c91b2b2860090e0d2625f251f2f9d19
Zavire Shiran <zavire.shiran@gmail.com>
2017-12-05 20:31:25 -0600

    switch eof handling to use match rather than input length check

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,18 @@ use std::io::{Write};
 
 fn main() {
     let mut stdout = io::stdout();
+    let stdin = io::stdin();
 
     loop {
         stdout.write(b"$ ").is_ok();
         stdout.flush().is_ok();
 
         let mut input = String::new();
-        io::stdin().read_line(&mut input).is_ok();
-        if input.len() == 0 {
-            stdout.write(b"\n").is_ok();
-            break
+        match stdin.read_line(&mut input) {
+            Ok(0) => break,
+            _ => {}
         }
+
         stdout.write(input.as_bytes()).is_ok();
     }
 }
