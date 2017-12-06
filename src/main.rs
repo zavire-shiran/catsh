e6b73162e0d2808889d340ff6202374e387024d2
Zavire Shiran <zavire.shiran@gmail.com>
2017-12-05 19:41:36 -0600

    add .expect() calls to resolve warnings

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ use std::io::{Write};
 
 fn main() {
     let mut stdout = io::stdout();
-    stdout.write(b"$ ");
-    stdout.flush();
+    stdout.write(b"$ ").expect("");
+    stdout.flush().expect("");
     let mut input = String::new();
-    io::stdin().read_line(&mut input);
+    io::stdin().read_line(&mut input).expect("");
     println!("{}", input);
 }
